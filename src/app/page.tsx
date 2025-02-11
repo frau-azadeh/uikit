2ef40027d13429e1a11e7947b725059e0688db23
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-10 23:53:03 -0800

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 "use client"
 
 import { Button } from "@/components/ui/Button"
-import { ShoppingCart } from "lucide-react";
 
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 p-10">
-      <Button variant="primary" size="md">دکمه اصلی</Button>
-      <Button variant="secondary" size="lg" icon={<ShoppingCart />}>افزودن به سبد</Button>
-      <Button variant="outline" size="sm" isLoading>در حال پردازش...</Button>
-      <Button variant="primary" ctaText="بیشتر بدانید" ctaVariant="secondary" onCtaClick={() => alert("CTA کلیک شد!")}>اطلاعات بیشتر</Button>
-
+      <Button>دکمه اصلی</Button>
+      
     </div>
   );
 }
