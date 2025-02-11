a919b73eadff8255c47f34377f261f2079d2f685
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-11 00:59:43 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,9 @@ import { Button } from "@/components/ui/Button";
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 p-10">
-      {/* دکمه‌ها زیر هم هستند ولی فاصله دارند */}
       <div className="flex flex-col gap-10  ">
-        <Button variant="primary" size="sm" align="right">دکمه اصلی</Button>
-        <Button variant="secondary" size="md" align="center">تماس با ما</Button>
+        <Button variant="primary" size="sm" align="right" width="small">دکمه اصلی</Button>
+        <Button variant="secondary" size="md" align="center" width="medium">تماس با ما</Button>
       </div>
     </div>
   );
