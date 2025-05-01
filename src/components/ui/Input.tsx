8a5c95005b14d72da201fcc91223491d54158187
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-01 16:01:20 +0330

    update Input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,43 +1,40 @@
-import { InputHTMLAttributes, ReactNode } from "react"
+import { cn } from '@/utils/cn';
+import { forwardRef, InputHTMLAttributes, useId } from 'react'
 
-interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>,"size">{
-    label ?: string;
-    type ?: "text" | "password" | "email" | "number";
-    inputSize ?: "sm" | "md" | "lg";
-    icon?: ReactNode;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+    label?: string;
     error?: string;
-    fullWidth?: boolean;
 }
 
-export const Input = ({
+const Input = forwardRef<HTMLInputElement, InputProps> (({
     label,
-    type = "text",
-    inputSize = "md",
-    icon,
     error,
-    fullWidth = false,
-    ...props
-}: InputProps) =>{
-    return(
-        <div className={`flex flex-col ${fullWidth ? "w-full" : "w-auto"}gap-1`}>
-                {label && <label className="text-gray-700 text-sm">{label}</label>}
-            <div className="relative flex items-center">
-                {icon && <span className="absolute left-3 text-gray-500">{icon}</span>}
-                
-                <input
-                    type={type}
-                    className={`border rounded px-3 py-2 transition w-full 
-                        ${icon? "pl-10": ""}
-                        ${inputSize === "sm"? "text-sm py-1":
-                          inputSize === "lg"? "text-lg py-3":
-                          "text-md py-2"}
-                        ${error?"border-red-500":"border-gray-300 focus:border-blue-500"}
-                        `}
-                        {...props}
-                />
-            </div>
-            {error &&<span className="text-red-500 text-xs">{error}</span>}
-        </div>
+    name,
+    className,
+    ...rest
+},ref) => {
+    const internalId = useId();
+    const inputId = name || internalId;
 
-    )
+  return (
+    <div className='w-full'>
+        {label && <label htmlFor={inputId} className='block mb-1 text-sm font-bold'>{label}</label>}
+        <input
+            id={internalId}
+            ref={ref}
+            name={name}
+            {...rest}
+            className={cn(
+                "border p-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400 transition",
+                error ? "border-red-500" : "border-gray-300",
+                className
+            )}
+        />
+        {error && <p className='text-red-500 test-sm mt-1'>{error}</p>}
+    </div>
+  )
 }
+)
+
+Input.displayName = 'Input';
+export default Input
