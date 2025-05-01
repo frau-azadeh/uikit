6744cf4d82b2f0a7e6536b7a3cc765b96a59ad6c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-01 18:01:10 +0330

    add export default Select Component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export {default as Button} from './Button';
 export {default as Index} from './Input';
 export {default as Pagination} from './Pagination';
+export {default as Select} from './Select';
