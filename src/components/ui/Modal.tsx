1e41ace9e64c7f3f447f1ba86916c6584d9ba46a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-01 19:22:41 +0330

    add Modal Component

---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,66 @@
+import React, { ReactNode, useEffect, useRef } from 'react'
+import Button from './Button';
+import { cn } from '@/utils/cn';
+
+interface ModalProps{
+    isOpen:boolean;
+    onClose: ()=> void;
+    children: ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    children
+}) => {
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    // ESC to close
+    useEffect(()=>{
+        const handleEsc = (e:KeyboardEvent)=>{
+            if(e.key === 'Escape') onClose();
+        };
+        document.addEventListener('keydown', handleEsc);
+        return ()=> document.removeEventListener('keydown', handleEsc);
+    }, [onClose]);
+
+    // Prevent background scroll
+    useEffect(()=>{
+        if(isOpen) document.body.style.overflow = 'hidden';
+        else document.body.style.overflow = '';
+        return () =>{ document.body.style.overflow = '';}
+    },[isOpen]);
+    //click outside to close
+    const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) =>{
+        if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+            onClose();
+        }
+    }
+    if(!isOpen) return null;
+  return (
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm'
+         onClick={handleBackdropClick}
+    >
+        <div className={cn(
+            'bg-white rounded-lg shadow-lg p-6 w-full max-w-lg reletive',
+            'animate-fadeIn'
+        )}>
+            <div className='flex justify-between items-center mb-4'>
+                <Button
+                    onClick={onClose}
+                    variant='outline'
+                    size='sm'
+                    className='p-1'
+                    aria-label='close Modal'
+                    >
+                        ✕
+                </Button>
+            </div>
+                {children}
+        </div>
+    </div>
+    
+  )
+}
+
+export default Modal
